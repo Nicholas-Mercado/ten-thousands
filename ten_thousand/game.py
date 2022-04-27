d8b65d1fd68acf45abe8af97019b776d187be736
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-27 15:28:11 -0700

    created cheater method, passed 1 test

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -48,7 +48,8 @@ class Game:
             print(format_dice_roll)
             print("Enter dice to keep, or (q)uit:")
             user_input = input('> ').lower()
-            
+            # print(dice_roll)
+            # print(user_input)
             # Quit *************************************************
             if user_input == 'q':
                 print(f'Thanks for playing. You earned {self.banker.balance} points')
@@ -57,22 +58,28 @@ class Game:
             user_input = tuple(int(x) for x in user_input)
             num_of_dice -= len(user_input)
             shelved_points = GameLogic.calculate_score(user_input)
-            ### cheater logic goes here
-            self.banker.shelf(shelved_points)
             
-                                                        
-            print(f'You have {shelved_points} unbanked points and {num_of_dice} dice remaining\n(r)oll again, (b)ank your points or (q)uit:')
-            user_input = input('> ').lower()
-            
-            if user_input == 'r':
-                dice_roll = roller(num_of_dice)
-                format_dice_roll = f'*** {self.format_rolls(dice_roll)}***'
+            if not self.is_cheater(user_input, dice_roll):
+                print("Cheater!!! Or possibly made a typo...")
                 print(format_dice_roll)
-            if user_input == 'b':
-                self.banker.bank()
-            if user_input == 'q':
-                print(f'Thanks for playing. You earned {self.banker.balance} points')
-                return    
+                print("Enter dice to keep, or (q)uit:")
+                user_input = input('> ').lower()
+            else:
+                self.banker.shelf(shelved_points)
+                
+                                                            
+                print(f'You have {shelved_points} unbanked points and {num_of_dice} dice remaining\n(r)oll again, (b)ank your points or (q)uit:')
+                user_input = input('> ').lower()
+                
+                if user_input == 'r':
+                    dice_roll = roller(num_of_dice)
+                    format_dice_roll = f'*** {self.format_rolls(dice_roll)}***'
+                    print(format_dice_roll)
+                if user_input == 'b':
+                    self.banker.bank()
+                if user_input == 'q':
+                    print(f'Thanks for playing. You earned {self.banker.balance} points')
+                    return    
                 
             print(f'You banked {shelved_points} points in round {round_counter}\nTotal score is {self.banker.balance} points')
 
@@ -84,7 +91,25 @@ class Game:
             string += f'{number} '
         return string
 
-
+    def is_cheater(self, user_input , dice_roll ):
+        test_dice_roll = dice_roll
+        check_list = []
+        
+        for num in user_input:
+            is_in_list = False
+            for position in test_dice_roll:
+                if num == position:
+                    is_in_list = True
+                    index = test_dice_roll.index(position)
+                    
+                    test_dice_roll[index] = 0
+                    print(test_dice_roll)
+                    break
+            check_list.append(is_in_list)
+            
+        print(check_list)
+        return all(check_list) 
+      
 if __name__ == '__main__':
     game = Game()
     banker = Banker()
